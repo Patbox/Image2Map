e0fa308619eec0210e174e6478c10056e1bf4291
Setadokalo <darkenchanter1@gmail.com>
2021-03-18 15:28:33 -0500

    refactor, added scale settings and poster mode - Refactored the command registration to use just one command with a multimap subcommand, and be structured better - Improved suggestion provider logic - Added "Poster Mode" maps, which symbolize a large map set and when placed create the entire larger image automatically - Implemented Scale Mode, which controls how images are scaled to fit a map set with a different aspect ratio

--- src/main/java/space/essem/image2map/Image2Map.java
+++ src/main/java/space/essem/image2map/Image2Map.java
@@ -1,9 +1,17 @@
 package space.essem.image2map;
 
+import com.mojang.brigadier.LiteralMessage;
+import com.mojang.brigadier.arguments.BoolArgumentType;
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.item.FilledMapItem;
+import net.minecraft.nbt.IntTag;
+import net.minecraft.nbt.ListTag;
+import net.minecraft.server.network.ServerPlayerEntity;
+import org.jetbrains.annotations.NotNull;
 import space.essem.image2map.config.Image2MapConfig;
 import space.essem.image2map.renderer.MapRenderer;
 
+import java.awt.*;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
@@ -30,171 +38,338 @@ import net.minecraft.server.command.CommandManager;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.text.LiteralText;
 import net.minecraft.item.ItemStack;
-import net.minecraft.command.argument.NumberRangeArgumentType;
 import net.minecraft.entity.ItemEntity;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.util.math.Vec3d;
 import net.fabricmc.fabric.api.command.v1.CommandRegistrationCallback;
 
 public class Image2Map implements ModInitializer {
-    public static Image2MapConfig CONFIG = AutoConfig.register(Image2MapConfig.class, GsonConfigSerializer::new)
-            .getConfig();
+	public static Image2MapConfig CONFIG = AutoConfig.register(Image2MapConfig.class, GsonConfigSerializer::new)
+		.getConfig();
 
-    @Override
-    public void onInitialize() {
-        System.out.println("Loading Image2Map...");
+	@Override
+	public void onInitialize() {
+		System.out.println("Loading Image2Map...");
 
-        CommandRegistrationCallback.EVENT.register((dispatcher, dedicated) -> {
-            dispatcher.register(CommandManager.literal("mapcreate")
-                    .requires(source -> source.hasPermissionLevel(CONFIG.minPermLevel))
-                    .then(CommandManager.argument("mode", StringArgumentType.word()).suggests(new DitherModeSuggestionProvider())
-                    .then(CommandManager.argument("path", StringArgumentType.greedyString())
-                        .executes(this::createMap))));
-        });
-        CommandRegistrationCallback.EVENT.register((dispatcher, dedicated) -> {
-            dispatcher.register(CommandManager.literal("multimapcreate")
-                    .requires(source -> source.hasPermissionLevel(CONFIG.minPermLevel))
-                    .then(CommandManager.argument("mode", StringArgumentType.word()).suggests(new DitherModeSuggestionProvider())
-                    .then(CommandManager.argument("sizex", IntegerArgumentType.integer(1, 10))
-                    .then(CommandManager.argument("sizey", IntegerArgumentType.integer(1, 10))
-                    .then(CommandManager.argument("path", StringArgumentType.greedyString())
-                        .executes(this::createMaps))))));
-        });
-    }
+		CommandRegistrationCallback.EVENT.register((dispatcher, dedicated) ->
+			dispatcher.register(CommandManager.literal("mapcreate")
+				.requires(source -> source.hasPermissionLevel(CONFIG.minPermLevel))
+				.then(CommandManager.argument("mode", StringArgumentType.word())
+					.suggests(new DitherModeSuggestionProvider())
+					.then(CommandManager.argument("path", StringArgumentType.string())
+						.executes(ctx -> createMaps(MapGenerationContext.getBasicInfo(ctx)))
+					)
+				).then(CommandManager.literal("multi")
+					.requires(source -> source.hasPermissionLevel(CONFIG.minPermLevel))
+					.then(CommandManager.argument("mode", StringArgumentType.word())
+						.suggests(new DitherModeSuggestionProvider())
+						.then(CommandManager.argument("width", IntegerArgumentType.integer(1, 25))
+						.then(CommandManager.argument("height", IntegerArgumentType.integer(1, 25))
+						.then(CommandManager.argument("path", StringArgumentType.string())
+							.then(CommandManager.argument("scale", StringArgumentType.word())
+							.suggests(new ScaleSuggestionProvider())
+							.then(CommandManager.argument("makePoster", BoolArgumentType.bool())
+								.executes(ctx ->
+									createMaps(MapGenerationContext.getBasicInfo(ctx).getSize(ctx).getScaleMethod(ctx).getPosterfy(ctx))
+								)
+							)
+							.executes(ctx ->
+								createMaps(MapGenerationContext.getBasicInfo(ctx).getSize(ctx).getScaleMethod(ctx))
+							))
+							.executes(ctx -> createMaps(MapGenerationContext.getBasicInfo(ctx).getSize(ctx)))
+						)))
+					)
+				)
+			));
+	}
 
-    class DitherModeSuggestionProvider implements SuggestionProvider<ServerCommandSource> {
+	static class ScaleSuggestionProvider implements SuggestionProvider<ServerCommandSource> {
+		@Override
+		public CompletableFuture<Suggestions> getSuggestions(CommandContext<ServerCommandSource> context, SuggestionsBuilder builder) throws CommandSyntaxException {
+			if ("fit".startsWith(builder.getRemaining().toLowerCase()))
+				builder.suggest("fit");
+			if ("fill".startsWith(builder.getRemaining().toLowerCase()))
+				builder.suggest("fill");
+			if ("stretch".startsWith(builder.getRemaining().toLowerCase()))
+				builder.suggest("stretch");
+			return builder.buildFuture();
+		}
+	}
 
-        @Override
-        public CompletableFuture<Suggestions> getSuggestions(CommandContext<ServerCommandSource> context,
-                SuggestionsBuilder builder) throws CommandSyntaxException {
-            builder.suggest("none");
-            builder.suggest("dither");
-            return builder.buildFuture();
-        }
-        
-    }
+	static class DitherModeSuggestionProvider implements SuggestionProvider<ServerCommandSource> {
 
-    public enum DitherMode {
-        NONE,
-        FLOYD;
+		@Override
+		public CompletableFuture<Suggestions> getSuggestions(CommandContext<ServerCommandSource> context,
+															 SuggestionsBuilder builder) throws CommandSyntaxException {
+			if ("none".startsWith(builder.getRemaining().toLowerCase()))
+				builder.suggest("none");
+			if ("dither".startsWith(builder.getRemaining().toLowerCase()))
+				builder.suggest("dither");
+			return builder.buildFuture();
+		}
 
-        public static DitherMode fromString(String string) {
-            if (string.equalsIgnoreCase("NONE"))
-                return DitherMode.NONE;
-            else if (string.equalsIgnoreCase("DITHER") || string.equalsIgnoreCase("FLOYD"))
-                    return DitherMode.FLOYD;
-            throw new IllegalArgumentException("invalid dither mode");
-        }
-    }
+	}
 
-    private int createMap(CommandContext<ServerCommandSource> context) throws CommandSyntaxException {
-        ServerCommandSource source = context.getSource();
-        Vec3d pos = source.getPosition();
-        PlayerEntity player = source.getPlayer();
-        DitherMode mode = null;
-        String modeStr = StringArgumentType.getString(context, "mode");
-        try {
-            mode = DitherMode.fromString(modeStr);
-        } catch (IllegalArgumentException e) {
-            throw new SimpleCommandExceptionType(() -> "Invalid dither mode '" + modeStr + "'").create();
-        }
-        String input = StringArgumentType.getString(context, "path");
+	public enum DitherMode {
+		NONE,
+		FLOYD;
 
-        source.sendFeedback(new LiteralText("Generating image map..."), false);
-        BufferedImage image = getImage(input, source);
-        
-        
-        createAndGiveMap(source, pos, player, mode, image);
-        source.sendFeedback(new LiteralText("Done!"), false);
+		public static DitherMode fromString(String string) throws CommandSyntaxException {
+			if (string.equalsIgnoreCase("NONE"))
+				return DitherMode.NONE;
+			else if (string.equalsIgnoreCase("DITHER") || string.equalsIgnoreCase("FLOYD"))
+				return DitherMode.FLOYD;
+			throw new CommandSyntaxException(
+				new SimpleCommandExceptionType(new LiteralMessage("Invalid Dither mode '" + string + "'")),
+				new LiteralMessage("Invalid Dither mode '" + string + "'"));
+		}
+	}
 
-        return 1;
-    }
+	private static void scaleImage(BufferedImage output, BufferedImage input, Graphics g, boolean fitAll) {
+		double imgAspect = (double) input.getHeight() / input.getWidth();
 
-    private int createMaps(CommandContext<ServerCommandSource> context) throws CommandSyntaxException {
-        ServerCommandSource source = context.getSource();
-        Vec3d pos = source.getPosition();
-        PlayerEntity player = source.getPlayer();
-        
-        String modeStr = StringArgumentType.getString(context, "mode");
-        final int countX = IntegerArgumentType.getInteger(context, "sizex");
-        final int countY = IntegerArgumentType.getInteger(context, "sizey");
-        try {
-            DitherMode mode = DitherMode.fromString(modeStr);
-			String input = StringArgumentType.getString(context, "path");
+		int outputWidth = output.getWidth();
+		int outputHeight = output.getHeight();
 
+		double canvasAspect = (double) outputHeight / outputWidth;
+
+		int x1 = 0;
+		int y1 = 0;
+
+		// XOR conditionally negates the IF statement (A XOR true == !A, A XOR false == A)
+		if (canvasAspect > imgAspect ^ !fitAll) {
+			y1 = outputHeight;
+			outputHeight = (int) (outputWidth * imgAspect);
+			y1 = (y1 - outputHeight) / 2;
+		} else {
+			x1 = outputWidth;
+			outputWidth = (int) (outputHeight / imgAspect);
+			x1 = (x1 - outputWidth) / 2;
+		}
+		int x2 = outputWidth + x1;
+		int y2 = outputHeight + y1;
+
+		g.drawImage(input, x1, y1, x2, y2, 0, 0, input.getWidth(), input.getHeight(), null);
+	}
+
+	private int createMaps(MapGenerationContext context) throws CommandSyntaxException {
+		try {
+			ServerCommandSource source = context.getSource();
 			source.sendFeedback(new LiteralText("Generating image map..."), false);
-			BufferedImage image = getImage(input, source);
-			if (image == null)
+			BufferedImage sourceImage = getImage(context.path, source);
+			if (sourceImage == null)
 				return 0;
+			ServerPlayerEntity player = source.getPlayer();
 			new Thread(() -> {
-				int sectionWidth = image.getWidth() / countX;
-				int sectionHeight = image.getHeight() / countY;
-				for (int y = 0; y < countY; y++) {
-					for (int x = 0; x < countX; x++) {
-						BufferedImage subImage = image.getSubimage(x * sectionWidth, y * sectionHeight, sectionWidth, sectionHeight);
-						createAndGiveMap(source, pos, player, mode, subImage);
+				BufferedImage img = new BufferedImage(context.countX * 128, context.countY * 128, BufferedImage.TYPE_INT_ARGB);
+				Graphics2D graphics = img.createGraphics();
+				if (context.getScaleMode() == ScaleMode.STRETCH)
+					graphics.drawImage(sourceImage, 0, 0, img.getWidth(), img.getHeight(), null);
+				else if (context.getScaleMode() == ScaleMode.FIT)
+					scaleImage(img, sourceImage, graphics, true);
+				else if (context.getScaleMode() == ScaleMode.FILL)
+					scaleImage(img, sourceImage, graphics, false);
+				img.flush();
+				final int SECTION_SIZE = 128;
+				ListTag maps = new ListTag();
+				for (int y = 0; y < context.countY; y++) {
+					ListTag mapsY = new ListTag();
+					for (int x = 0; x < context.countX; x++) {
+						BufferedImage subImage = img.getSubimage(x * SECTION_SIZE, y * SECTION_SIZE, SECTION_SIZE, SECTION_SIZE);
+						ItemStack stack = createMap(source, context.mode, subImage);
+						if (context.shouldMakePoster() && (context.countX > 1 || context.countY > 1)) {
+							mapsY.add(IntTag.of(FilledMapItem.getMapId(stack)));
+						} else {
+							givePlayerMap(player, stack);
+						}
 					}
+					maps.add(mapsY);
+				}
+				if (context.shouldMakePoster()) {
+					ItemStack stack = createMap(source, context.mode, MapRenderer.convertToBufferedImage(
+						sourceImage.getScaledInstance(128, 128, Image.SCALE_DEFAULT)));
+					stack.putSubTag("i2mStoredMaps", maps);
+					givePlayerMap(player, stack);
 				}
 				source.sendFeedback(new LiteralText("Done!"), false);
 			}).start();
 			source.sendFeedback(new LiteralText("Map Creation Queued!"), false);
-		} catch (IllegalArgumentException e) {
-			throw new SimpleCommandExceptionType(() -> "Invalid dither mode '" + modeStr + "'").create();
 		} catch (Exception e) {
 			e.printStackTrace();
 			throw e;
 		}
-
-        return 1;
-    }
-
-    @Nullable
-    private BufferedImage getImage(String urlStr, ServerCommandSource source) {
-        BufferedImage image = null;
-        try {
-            if (isValid(urlStr)) {
-                URL url = new URL(urlStr);
-                URLConnection connection = url.openConnection();
-                connection.setRequestProperty("User-Agent", "Image2Map mod");
-                connection.connect();
-                image = ImageIO.read(connection.getInputStream());
-            } else if (CONFIG.allowLocalFiles) {
-                File file = new File(urlStr);
-                image = ImageIO.read(file);
-            } else {
-                image = null;
-            }
-        } catch (IOException e) {
-            source.sendFeedback(new LiteralText("That doesn't seem to be a valid image."), false);
-            return null;
-        }
-
-        if (image == null) {
-            source.sendFeedback(new LiteralText("That doesn't seem to be a valid image."), false);
-            return null;
-        }
-        return image;
-    }
-
-    private void createAndGiveMap(ServerCommandSource source, Vec3d pos, PlayerEntity player, DitherMode mode,
-            BufferedImage image) {
-        ItemStack stack = MapRenderer.render(image, mode, source.getWorld(), pos.x, pos.z, player);
-
-        if (!player.inventory.insertStack(stack)) {
-            ItemEntity itemEntity = new ItemEntity(player.world, player.getPos().x, player.getPos().y,
-                    player.getPos().z, stack);
-            player.world.spawnEntity(itemEntity);
-        }
-    }
+		return 1;
+	}
 
 
+	@Nullable
+	private BufferedImage getImage(String urlStr, ServerCommandSource source) {
+		BufferedImage image = null;
+		try {
+			if (isValid(urlStr)) {
+				URL url = new URL(urlStr);
+				URLConnection connection = url.openConnection();
+				connection.setRequestProperty("User-Agent", "Image2Map mod");
+				connection.connect();
+				image = ImageIO.read(connection.getInputStream());
+			} else if (CONFIG.allowLocalFiles) {
+				File file = new File(urlStr);
+				image = ImageIO.read(file);
+			} else {
+				image = null;
+			}
+		} catch (IOException e) {
+			source.sendFeedback(new LiteralText("That doesn't seem to be a valid image."), false);
+			return null;
+		}
 
-    private static boolean isValid(String url) {
-        try {
-            new URL(url).toURI();
-            return true;
-        } catch (Exception e) {
-            return false;
-        }
-    }
+		if (image == null) {
+			source.sendFeedback(new LiteralText("That doesn't seem to be a valid image."), false);
+			return null;
+		}
+		return image;
+	}
+
+	private ItemStack createMap(ServerCommandSource source, DitherMode mode,
+								BufferedImage image) {
+		return MapRenderer.render(image, mode, source.getWorld(), source.getPosition().x, source.getPosition().z);
+	}
+
+	private void givePlayerMap(PlayerEntity player, ItemStack stack) {
+		if (!player.inventory.insertStack(stack)) {
+			ItemEntity itemEntity = new ItemEntity(player.world, player.getPos().x, player.getPos().y,
+				player.getPos().z, stack);
+			player.world.spawnEntity(itemEntity);
+		}
+	}
+
+	private static boolean isValid(String url) {
+		try {
+			new URL(url).toURI();
+			return true;
+		} catch (Exception e) {
+			return false;
+		}
+	}
+
+	private static enum ScaleMode {
+		FIT,
+		FILL,
+		STRETCH;
+
+		public static ScaleMode fromString(String sMode) {
+			switch (sMode.toUpperCase()) {
+				case "FIT":
+					return FIT;
+				case "FILL":
+					return FILL;
+				case "STRETCH":
+					return STRETCH;
+				default:
+					throw new IllegalArgumentException("input string must be a valid enum value!");
+			}
+		}
+	}
+
+	private static class MapGenerationContext {
+		public ServerCommandSource getSource() {
+			return source;
+		}
+
+		public MapGenerationContext source(ServerCommandSource source) {
+			this.source = source;
+			return this;
+		}
+
+		ServerCommandSource source;
+		private DitherMode mode = DitherMode.FLOYD;
+		private ScaleMode scaleMode = ScaleMode.STRETCH;
+		private String path;
+
+		public boolean shouldMakePoster() {
+			return makePoster;
+		}
+
+		private boolean makePoster = true;
+		private int countX = 1;
+		private int countY = 1;
+
+		private static MapGenerationContext getBasicInfo(CommandContext<ServerCommandSource> context) throws CommandSyntaxException {
+			return new MapGenerationContext(StringArgumentType.getString(context, "path"))
+				.mode(DitherMode.fromString(StringArgumentType.getString(context, "mode")))
+				.source(context.getSource());
+		}
+
+		private MapGenerationContext getSize(CommandContext<ServerCommandSource> context) {
+			return this
+				.countX(IntegerArgumentType.getInteger(context, "width"))
+				.countY(IntegerArgumentType.getInteger(context, "height"));
+		}
+
+		private MapGenerationContext getScaleMethod(CommandContext<ServerCommandSource> context) {
+			return this
+				.scaleMode(ScaleMode.fromString(StringArgumentType.getString(context, "scale")));
+		}
+
+		public MapGenerationContext getPosterfy(CommandContext<ServerCommandSource> context) {
+			return this
+				.makePoster(BoolArgumentType.getBool(context, "makePoster"));
+		}
+
+		private MapGenerationContext makePoster(boolean makePoster) {
+			this.makePoster = makePoster;
+			return this;
+		}
+
+
+		public DitherMode getMode() {
+			return mode;
+		}
+
+		public MapGenerationContext mode(DitherMode mode) {
+			this.mode = mode;
+			return this;
+		}
+
+		public String getPath() {
+			return path;
+		}
+
+		public MapGenerationContext path(String path) {
+			this.path = path;
+			return this;
+		}
+
+		public int getCountX() {
+			return countX;
+		}
+
+		public MapGenerationContext countX(int countX) {
+			this.countX = countX;
+			return this;
+		}
+
+		public int getCountY() {
+			return countY;
+		}
+
+		public MapGenerationContext countY(int countY) {
+			this.countY = countY;
+			return this;
+		}
+
+		public MapGenerationContext(@NotNull String path) {
+			this.path = path;
+		}
+
+		public ScaleMode getScaleMode() {
+			return scaleMode;
+		}
+
+		public MapGenerationContext scaleMode(ScaleMode scaleMode) {
+			this.scaleMode = scaleMode;
+			return this;
+		}
+
+	}
+
 }
---
+++ src/main/java/space/essem/image2map/mixin/ItemFrameMixin.java
@@ -0,0 +1,126 @@
+package space.essem.image2map.mixin;
+
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.decoration.AbstractDecorationEntity;
+import net.minecraft.entity.decoration.ItemFrameEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.nbt.ListTag;
+import net.minecraft.text.LiteralText;
+import net.minecraft.util.math.Box;
+import net.minecraft.util.math.Direction;
+import net.minecraft.util.math.Vec3d;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.EnumSet;
+import java.util.List;
+
+@Mixin(ItemFrameEntity.class)
+public abstract class ItemFrameMixin extends AbstractDecorationEntity {
+	private ItemFrameMixin(EntityType<? extends AbstractDecorationEntity> entityType) {
+		super(entityType, null);
+		throw new IllegalStateException("this should never be called!");
+	}
+
+	@Inject(method = "setHeldItemStack(Lnet/minecraft/item/ItemStack;Z)V", at =
+		@At(value = "INVOKE", shift = At.Shift.AFTER, target = "net/minecraft/world/World.updateComparators(Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/Block;)V"))
+	private void checkForPosterMap(ItemStack value, boolean update, CallbackInfo ci) {
+		if (value.getItem() != Items.FILLED_MAP ||
+			  value.getTag() == null ||
+			  this.facing.getAxis().isVertical() ||
+			  !value.getTag().contains("i2mStoredMaps", 9) ||
+			  value.getTag().getList("i2mStoredMaps", 9).get(0).getType() != 9
+		) return;
+		ListTag maps = (ListTag) value.getTag().get("i2mStoredMaps");
+		if (maps != null) {
+			int hSize = ((ListTag)maps.get(0)).size();
+			int vSize = maps.size();
+			Vec3d origin = this.getPos();
+			Vec3d hVec = Vec3d.of(facing.rotateYCounterclockwise().getVector());
+			origin = verifyOrigin(hSize, origin, hVec);
+			if (origin == null) {
+				value.setCustomName(new LiteralText("Invalid Item Frame Structure"));
+				return;
+			}
+			origin = verifyOrigin(vSize, origin, new Vec3d(0.0, 1.0, 0.0));
+			if (origin == null) {
+				value.setCustomName(new LiteralText("Invalid Item Frame Structure"));
+				return;
+			}
+			ItemFrameEntity[][] posterFrames = new ItemFrameEntity[vSize][hSize];
+			for (int y = 0; y < vSize; y++) {
+				for (int x = 0; x < hSize; x++) {
+					ItemFrameEntity frame = this.getAlignedFrameAt(
+						  origin.add(hVec.multiply(x))
+								.add(0.0, y, 0.0));
+					if (frame == null || frame.getHeldItemStack().isEmpty()) {
+						value.setCustomName(new LiteralText("Invalid Item Frame Structure"));
+						return;
+					}
+					posterFrames[y][x] = frame;
+				}
+			}
+			for (int y = 0; y < vSize; y++) {
+				if (!(maps.get(y) instanceof ListTag) || ((ListTag) maps.get(y)).size() < vSize)
+				for (int x = 0; x < hSize; x++) {
+					ItemStack frameStack = new ItemStack(Items.FILLED_MAP, 1);
+					frameStack.putSubTag("map", ((ListTag)maps.get(y)).get(x));
+					posterFrames[vSize - y - 1][x].setHeldItemStack(frameStack, true);
+				}
+			}
+			System.out.println("Magic Map put in item frame!\n");
+			System.out.println("Origin Pos: " + origin);
+			System.out.println("Image Size is: " + hSize + "x" + vSize);
+			System.out.println("Facing " + this.facing.toString());
+
+		}
+	}
+
+	/**
+	 * Ensures there is at least `target` item frames in a row, returning the position of the first one in the row.
+	 * @param target the target number of item frames
+	 * @param origin the initial position for the search
+	 * @param searchDir the direction to move each search
+	 * @return the position of the first item frame.
+	 */
+	private Vec3d verifyOrigin(int target, Vec3d origin, Vec3d searchDir) {
+		int found = 0;
+		// ensure origin is unique since we mutate it
+		origin = new Vec3d(origin.x, origin.y, origin.z);
+		Vec3d searchPos = origin;
+		boolean reverseSearch = false; // when we run out of item frames in forward search we reverse to find the new origin
+		while (found < target) {
+			if (!reverseSearch) {
+				List<ItemFrameEntity> itemFramesInBlock = world.getEntitiesByType(EntityType.ITEM_FRAME, Box.method_29968(searchPos.floorAlongAxes(EnumSet.of(
+					  Direction.Axis.X, Direction.Axis.Y, Direction.Axis.Z))), entity -> true);
+				ItemFrameEntity alignedFrame = this.getAlignedFrameAt(searchPos);
+				if (alignedFrame != null && alignedFrame.getHeldItemStack().isEmpty()) {
+					searchPos = searchPos.add(searchDir);
+					found++;
+				} else {
+					reverseSearch = true;
+				}
+			} else {
+				//start searching left from the origin pos minus one (since we already searched the origin pos)
+				// we also move the origin left because we want the detected maps to be between originPos and searchPos
+				origin = origin.subtract(searchDir);
+				if (this.getAlignedFrameAt(origin) != null) {
+					searchPos = searchPos.subtract(searchDir);
+					found++;
+				} else break;
+			}
+		}
+		return found == target ? origin : null;
+	}
+	private ItemFrameEntity getAlignedFrameAt(Vec3d pos) {
+		List<ItemFrameEntity> itemFramesInBlock = world.getEntitiesByType(
+			  EntityType.ITEM_FRAME,
+			  Box.method_29968(pos.floorAlongAxes(EnumSet.of(
+			  	  Direction.Axis.X, Direction.Axis.Y, Direction.Axis.Z))),
+			  entity -> entity.getHorizontalFacing().equals(this.getHorizontalFacing()));
+		return itemFramesInBlock.size() > 0 ? itemFramesInBlock.get(0) : null;
+	}
+}
